6f8fb64201b84c584ad6e80ed40913b2e567707b
GianluccaPaiva <gianluccapaiva@gmail.com>
2026-03-07 09:17:34 -0300

    feat: add hooks for managing configurations, mural posts, and search functionality

---
+++ hooks/leituraJson.ts
@@ -0,0 +1,19 @@
+import turmasJson from "@/dados/turmas.json";
+
+export type TurmaProps = {
+  materia: string;
+  professor: string;
+  banners: string;
+  alunos: string[];
+  foto_professor: string;
+  sala: string;
+  turma: string;
+};
+
+export type EscolaProps = {
+  escola: string;
+  ano_letivo: number;
+  turmas: Record<string, TurmaProps>;
+};
+
+export const listaEscolar = turmasJson as EscolaProps;
---
+++ hooks/useMural.ts
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+export type Post = {
+    id: string;
+    conteudo: string;
+    data: string;
+};
+
+export function useMural() {
+    const [posts, setPosts] = useState<{ posts: Post[]; boxAberto: boolean; tipoAmostar:"atividade"|"mural"|"contato"|"alunos" }>({
+        posts: [],
+        boxAberto: false,
+        tipoAmostar: "mural",
+    });
+    const [conteudo, setConteudo] = useState("");
+    const [assunto, setAssunto] = useState("");
+
+    const mudarAberturaBox = (boxAberto: boolean) =>
+        setPosts((anterior) => ({ ...anterior, boxAberto }));
+
+    const handlePublicar = () => {
+        if (conteudo.trim()) {
+            const novoPost: Post = {
+                id: Date.now().toString(),
+                conteudo,
+                data: new Date().toLocaleString("pt-BR"),
+            };
+            setPosts((anterior) => ({
+                ...anterior,
+                posts: [...anterior.posts, novoPost],
+            }));
+            setConteudo("");
+            mudarAberturaBox(false);
+            abrirMural();
+        }
+    };
+
+    const handleCancelar = () => {
+        setConteudo("");
+        setAssunto("");
+        mudarAberturaBox(false);
+        abrirMural();
+    };
+
+    const abrirMural = () => {
+        setPosts((anterior) => ({ ...anterior, tipoAmostar: "mural" }));
+    };
+
+    const abrirAtividades = () => {
+        setPosts((anterior) => ({ ...anterior, tipoAmostar: "atividade" }));
+    };
+
+    const abrirContato = () => {
+        setPosts((anterior) => ({ ...anterior, tipoAmostar: "contato", boxAberto: true }));
+    };
+
+    const abrirMensagemContato = () => {
+        setConteudo("");
+        setAssunto("");
+        setPosts((anterior) => ({ ...anterior, boxAberto: false }));
+        abrirMural();
+    }
+
+    const abrirAlunos = () => {
+        setPosts((anterior) => ({ ...anterior, tipoAmostar: "alunos" }));
+    }
+
+
+    return {
+        posts,
+        conteudo,
+        setConteudo,
+        assunto,
+        setAssunto,
+        mudarAberturaBox,
+        handlePublicar,
+        handleCancelar,
+        abrirMural,
+        abrirAtividades,
+        abrirContato,
+        abrirMensagemContato,
+        abrirAlunos,
+    };
+}
