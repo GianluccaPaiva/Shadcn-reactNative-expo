a2c810571fed82280b42fd027c2288282bb5eabb
GabrielLineker <gabriel.lineker@estudante.ufjf.br>
2026-03-07 09:20:38 -0300

    feat: implement linear gradient background in App component and update Tailwind CSS configuration

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import { Button } from '@/components/ui/button';
 import { Text } from '@/components/ui/text';
+import { LinearGradient } from 'expo-linear-gradient';
 export default function App() {
-return (
-<Button>
-<Text>Meu Botão Nativo!</Text>
-</Button>
-);
+  return (
+    <LinearGradient
+      colors={['#8B5CF6', '#EC4899']}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      className="p-4 rounded-xl"
+    >
+      <Text className="text-white font-bold">Botão com Gradiente Nativo!</Text>
+    </LinearGradient>
+  );
 }
